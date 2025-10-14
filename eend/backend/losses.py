b43c95075bf9e96d16943f39555d028cc18daad0
SEG-EEND <mcy2005@naver.com>
2025-10-14 14:10:01 +0900

    Update losses.py

--- eend/backend/losses.py
+++ eend/backend/losses.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Copyright 2019 Hitachi, Ltd. (author: Yusuke Fujita)
-# Copyright 2025 C. Moon
+# Copyright 2025 Human Interface Lab (author: C. Moon)
 # Licensed under the MIT license.
 
 from itertools import permutations
