824bcd393197b10d2c3295cf985d3521b7e73869
wade <>
2025-10-29 20:33:43 +0900

    Enable cuDNN runtime

--- eend/train.py
+++ eend/train.py
@@ -699,7 +699,7 @@ if __name__ == '__main__':
     torch.cuda.manual_seed_all(args.seed)  # if you are using multi-GPU.
     np.random.seed(args.seed)  # Numpy module.
     random.seed(args.seed)  # Python random module.
-    torch.backends.cudnn.enabled = False
+    torch.backends.cudnn.enabled = True
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
     os.environ['PYTHONHASHSEED'] = str(args.seed)
